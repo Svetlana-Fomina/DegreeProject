d1ce7cc8dd2717f61e5821d29dd1cb11ca3e717b
Svetlana-Fomina <147494007+Svetlana-Fomina@users.noreply.github.com>
2023-10-10 12:17:45 +0300

    configuration.py

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://47525ced-5beb-4179-8c88-3c8f3ff3dec6.serverhub.praktikum-services.ru"   # url-стенда
+DOC_PATH = "/docs/"                                                                            # путь до документации API
+CREATE_ORDER = "/api/v1/orders"                                                                # путь до документации на создание заказа
+GET_ORDER_TRACK = "/api/v1/orders/track"                                                       # путь до документации на получение заказа по трек-номеру
