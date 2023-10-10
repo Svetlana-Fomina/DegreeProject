4d7ac87ad7e101ea5c2ba4b1b0653a2d64e56dea
Svetlana-Fomina <147494007+Svetlana-Fomina@users.noreply.github.com>
2023-10-10 12:18:45 +0300

    data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+# Тело запроса при создании заказа
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Ивавнов",
+    "address": "ул. Адмирала Руднева, 2",
+    "metroStation": 205,
+    "phone": "+34916123451",
+    "rentTime": 1,
+    "deliveryDate": "2023-09-09",
+    "comment": "Привет, Милка!",
+    "color": ["BLACK"
+    ]
+}
